b33f85d872c34ed44d18e5bfd188b55c742d862b
Alkan-09 <98907148+Alkan-09@users.noreply.github.com>
2022-11-08 19:21:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function(){
     function startTime() {
-        var today = new Date();
-        var hr = today.getHours();
-        var min = today.getMinutes();
-        var sec = today.getSeconds();
+        let today = new Date();
+        let hr = today.getHours();
+        let min = today.getMinutes();
+        let sec = today.getSeconds();
         ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
         hr = (hr == 0) ? 12 : hr;
         hr = (hr > 12) ? hr - 12 : hr;
@@ -13,16 +13,16 @@ window.onload = function(){
         sec = checkTime(sec);
         document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
         
-        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        var curWeekDay = days[today.getDay()];
-        var curDay = today.getDate();
-        var curMonth = months[today.getMonth()];
-        var curYear = today.getFullYear();
-        var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let curWeekDay = days[today.getDay()];
+        let curDay = today.getDate();
+        let curMonth = months[today.getMonth()];
+        let curYear = today.getFullYear();
+        let date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
         document.getElementById("date").innerHTML = date;
         
-        var time = setTimeout(function(){ startTime() }, 500);
+        let time = setTimeout(function(){ startTime() }, 500);
     }
     function checkTime(i) {
         if (i < 10) {
